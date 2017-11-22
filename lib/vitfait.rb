c01e9be3cfd4f796a4d2fbe4dbc25689fd299490
VictorTpo <victor.thepaut@gmail.com>
2017-11-22 18:47:03 +0100

    init

---
+++ lib/vitfait.rb
@@ -0,0 +1,18 @@
+class Vitfait
+  def happy_end
+    puts "\n=========================="
+    puts 'Done, you deserve it : 🍺'
+    puts '==========================='
+  end
+
+  def draw_progress(progress_current, progress_total)
+    percentage_done = ((progress_current + 1) * 100) / progress_total
+    percentage_left = 100 - percentage_done
+    print "\r"
+    print "In progress => #{progress_current + 1} / #{progress_total} | "
+    done = '_' * percentage_done
+    left = '_' * (percentage_left - 1) if percentage_left > 0
+    print "#{percentage_done}% "
+    print "#{done}🚋 #{left} 🏁  "
+  end
+end
---
+++ vitfait.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name        = 'vitfait'
+  s.version     = '0.0.0'
+  s.date        = '2017-11-22'
+  s.summary     = 'Is it done ?'
+  s.description = 'Just to show the progression'
+  s.authors     = ['Victor Thepaut']
+  s.email       = 'victor.thepaut@gmail.com'
+  s.files       = ['lib/vitfait.rb']
+  s.license     = 'MIT'
+end
